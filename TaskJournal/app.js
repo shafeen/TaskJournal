24a17f4e4b55eda3f43fe565a1b07062e9f8a10e
Shafeen M <sodl02@gmail.com>
2017-06-02 14:27:12 -0400

    serving all static files under public

--- TaskJournal/app.js
+++ TaskJournal/app.js
@@ -25,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+// all of the folder paths under public will be available via "localhost:3000/"
+app.use(express.static('public'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
