14c8247212561c6c2668152d2c732391e5f287cc
Jake Anderson <barykade@umich.edu>
2017-06-02 15:05:14 -0400

    tags get added and deleted to tagsArray successfully

--- TaskJournal/routes/route-controllers/TagsControllers.js
+++ TaskJournal/routes/route-controllers/TagsControllers.js
@@ -1,18 +1,79 @@
+class Tag {
+    constructor(id, description){
+        this.id = id;
+        this.description = description;
+    }
+}
+
+var tagsArray = [];
+
 var TagsAddTagController = function (req, res) {
-    var taskID = req.params.task_id;
+    var taskId = req.params.task_id;
     var tagDescription = req.body.tag_description;
-    console.log("task ID: %s", taskID);
-    console.log("tag description: %s", tagDescription);
-    res.send('add tag');
+
+    var tagId = insertTagIntoTagsArray(tagDescription);
+
+    console.log(tagsArray);
+    res.send('Tag Added Successfully');
 };
 
 var TagsDeleteTagController = function (req, res) {
-    var taskID = req.params.task_id;
-    var tagID = req.body.tag_id;
-    console.log("task ID: %s", taskID);
-    console.log("tag ID: %s", tagID);
-    res.send('delete tag');
+    var taskId = req.params.task_id;
+    var tagId = req.body.tag_id;
+
+    deleteTagFromTagsArray(tagId);
+
+    console.log(tagsArray);
+    res.send('Tag Deleted Successfully');
 };
+
+function insertTagIntoTagsArray(description){
+    var tagID;
+    if (tagDescriptionAlreadyExists(description)){
+        tagID = getTagIdForTagDescription(description);
+    }else{
+        tagID = getNextAvailableTagId();
+        tagsArray.push(new Tag(tagID, description));
+    }
+    return tagID
+}
+
+function deleteTagFromTagsArray(tagId){
+    tagsArray.forEach(function (tag) {
+        if (tag.id == tagId){
+            var index = tagsArray.indexOf(tag);
+            if (index >= 0 && index < tagsArray.length){
+                tagsArray.splice(index, 1);
+            }
+        }
+    })
+}
+
+function tagDescriptionAlreadyExists(description){
+    tagsArray.forEach(function (tag) {
+        if (tag.description == description){
+            return true;
+        }
+    })
+    return false;
+}
+
+function getTagIdForTagDescription(description){
+    tagsArray.forEach(function (tag) {
+        if (tag.description == description){
+            return tag.id;
+        }
+    })
+    return null;
+}
+
+function getNextAvailableTagId(){
+    if (tagsArray.length == 0) {
+        return 0;
+    }else{
+        return tagsArray[tagsArray.length - 1].id + 1;
+    }
+}
 
 module.exports = {
     TagsAddTagController: TagsAddTagController,
