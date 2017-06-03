a621d304087af52b102ed878d7532998fbc61858
Shafeen M <sodl02@gmail.com>
2017-06-03 15:25:24 -0400

    simplified task and tag route controller usage

--- TaskJournal/routes/index.js
+++ TaskJournal/routes/index.js
@@ -1,33 +1,22 @@
 let express = require('express');
 let router = express.Router();
-let SampleCreateRouteController = require('./route-controllers/SampleRouteControllers.js').SampleCreateRouteController;
-let SampleDeleteRouteController = require('./route-controllers/SampleRouteControllers.js').SampleDeleteRouteController;
-let TagsAddTagController = require('./route-controllers/TagsControllers.js').TagsAddTagController;
-let TagsDeleteTagController = require('./route-controllers/TagsControllers.js').TagsDeleteTagController;
-let TagsGetTagsController = require('./route-controllers/TagsControllers.js').TagsGetTagsController;
-let TaskCreateRouteController = require('./route-controllers/TaskRouteControllers.js').TaskCreateController;
-let TaskDeleteRouteController = require('./route-controllers/TaskRouteControllers.js').TaskDeleteController;
-let TaskModifyRouteController = require('./route-controllers/TaskRouteControllers.js').TaskModifyController;
-let GetTasksController = require('./route-controllers/TaskRouteControllers.js').GetAllTasksController;
-let GetTasksByDateController = require('./route-controllers/TaskRouteControllers.js').GetTasksByDateController;
 
+let TagsRouteControllers = require('./route-controllers/TagsControllers.js');
+let TaskRouteControllers = require('./route-controllers/TaskRouteControllers.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Task Journal' });
 });
 
-router.post('/sample/create', SampleCreateRouteController);
-router.post('/sample/delete', SampleDeleteRouteController);
+router.get('/tags', TagsRouteControllers.TagsGetTagsController);
+router.post('/tags/add/task/:task_id', TagsRouteControllers.TagsAddTagController);
+router.post('/tags/delete/task/:task_id', TagsRouteControllers.TagsDeleteTagController);
 
-router.post('/tags/add_tag/task/:task_id', TagsAddTagController);
-router.post('/tags/delete_tag/task/:task_id', TagsDeleteTagController);
-router.get('/tags', TagsGetTagsController);
-
-router.post('/task/create', TaskCreateRouteController);
-router.post('/task/delete', TaskDeleteRouteController);
-router.post('/task/modify', TaskModifyRouteController);
-router.get('/task', GetTasksController);
-router.get('/task/date/:date', GetTasksByDateController);
+router.post('/task/create', TaskRouteControllers.TaskCreateController);
+router.post('/task/delete', TaskRouteControllers.TaskDeleteController);
+router.post('/task/modify', TaskRouteControllers.TaskModifyController);
+router.get('/task', TaskRouteControllers.GetAllTasksController);
+router.get('/task/date/:date', TaskRouteControllers.GetTasksByDateController);
 
 module.exports = router;
