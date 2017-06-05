4de92ebede0fa3ad7c3e2f830beb15b6db5d2671
Shafeen M <sodl02@gmail.com>
2017-06-05 01:01:33 -0400

    added a basic weekly layout in the landing page

--- TaskJournal/public/javascripts/initialize.js
+++ TaskJournal/public/javascripts/initialize.js
@@ -2,16 +2,27 @@ let dateUtil = function () {
     return {
         getDateStr_YYYYMMDD: function (dateObj) {
             return `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDate()}`;
+        },
+        getPanelSelectorForDay: function (date) {
+            return `#day${date.getDay()}`;
         }
-    }
+    };
 }();
 
 
 $(function () {
-    console.log('this is now working');
+    let today = new Date();
+    let todayStr = dateUtil.getDateStr_YYYYMMDD(today);
+    console.log("today is %s", todayStr);
 
-    let today = dateUtil.getDateStr_YYYYMMDD(new Date());
+    // hide all panels besides todays
+    $('.panel-body').hide();
+    $(dateUtil.getPanelSelectorForDay(today))
+        .removeClass('panel-default')
+        .addClass('panel-primary')
+        .find('.panel-body').show();
 
+    // init today's panel
 
 
 });
