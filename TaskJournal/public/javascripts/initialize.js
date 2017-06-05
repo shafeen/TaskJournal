9929990d58a24c3b29b57191af765055cf699b7c
Shafeen M <sodl02@gmail.com>
2017-06-05 01:30:49 -0400

    weekly layout now adds in the current day's tasks (on refresh)

--- TaskJournal/public/javascripts/initialize.js
+++ TaskJournal/public/javascripts/initialize.js
@@ -1,7 +1,7 @@
 let dateUtil = function () {
     return {
         getDateStr_YYYYMMDD: function (dateObj) {
-            return `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDate()}`;
+            return `${dateObj.getFullYear()}-${dateObj.getMonth()+1}-${dateObj.getDate()}`;
         },
         getPanelSelectorForDay: function (date) {
             return `#day${date.getDay()}`;
@@ -9,20 +9,32 @@ let dateUtil = function () {
     };
 }();
 
+function addTaskToList(date, task) {
+    let panelListSelector = `${dateUtil.getPanelSelectorForDay(date)} ul`;
+    $(panelListSelector).append($(`<li>${task.description}</li>`));
+}
 
 $(function () {
     let today = new Date();
     let todayStr = dateUtil.getDateStr_YYYYMMDD(today);
     console.log("today is %s", todayStr);
 
-    // hide all panels besides todays
+    // hide all panels besides today's
     $('.panel-body').hide();
     $(dateUtil.getPanelSelectorForDay(today))
         .removeClass('panel-default')
         .addClass('panel-primary')
         .find('.panel-body').show();
 
-    // init today's panel
+    // initialize today's panel
+    let todaysTasks = [];
+    $.get(`/task/date/${todayStr}`).done((response) => {
+        console.log(response);
+        todaysTasks = response;
+        todaysTasks.forEach((task) => {
+            addTaskToList(today, task);
+        })
+    });
 
 
 });
